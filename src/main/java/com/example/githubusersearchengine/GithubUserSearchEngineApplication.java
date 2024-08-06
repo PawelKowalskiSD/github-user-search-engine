30e3e6f8bd95edacc157de85b2ab5592e97eaedc
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-08-06 23:01:46 +0200

    v1.0.1

---
+++ src/main/java/com/example/githubusersearchengine/GithubUserSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubusersearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUserSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubusersearchengine/GithubUserSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubusersearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
